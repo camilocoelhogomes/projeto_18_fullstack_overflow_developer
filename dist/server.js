63776b0111706b218d017a6206ed060ad3c5dca8
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-12-13 13:06:31 -0300

    using the heroku port

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import './setup.js';
 import app from './app.js';
-const port = 4000;
+const port = Number(process.env.PORT);
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 import './setup';
 import app from './app';
 
-const port = 4000;
+const port = Number(process.env.PORT);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
