51ba88612767168aed7689a38a8e03564f7fa8f3
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-12-13 12:00:45 -0300

    build the last .js

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import './setup.js';
-import app from './app';
+import app from './app.js';
 const port = 4000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
